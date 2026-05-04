1775ae3a737e0879b1aa3a4df1b09d46bed48221
schicgirl-hair <contacte.schicgirl@gmail.com>
2026-05-04 12:30:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SCHICCHAT_CONFIG = {
   shopLink: "https://selar.com/hydratation-cheveux-crepus",
-  premiumLink: "https://selar.com/1t7713181a",
+  premiumLink: "https://selar.com/70t3ur414k",
   
   customFinal: "💛 Choisis l’offre qui correspond le mieux à ton besoin actuel.",
   leadStorageKey: "schicchat_coach_leads",
